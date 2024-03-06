33d09a62a2d7bbe57257f75e0b99bd1d9427b19a
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-05 18:13:54 -0800

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("level", true)]
+        [InlineData("radar", true)]
 
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            //Arrange
+            WordSmith instance = new WordSmith();
+
+            //Act
+            bool actual = instance.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
